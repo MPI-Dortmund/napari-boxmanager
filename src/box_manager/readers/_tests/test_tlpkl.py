7081ec5dd46ad99df7c13912f0257c806f3485d8
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2022-06-29 16:34:10 +0200

    Add more tests

--- src/box_manager/readers/_tests/test_tlpkl.py
+++ src/box_manager/readers/_tests/test_tlpkl.py
@@ -2,6 +2,7 @@ import os
 import pathlib
 
 import numpy
+import pandas
 import pytest
 
 import box_manager.readers.tlpkl as nrt
@@ -9,168 +10,272 @@ import box_manager.readers.tlpkl as nrt
 TEST_DATA = pathlib.Path(os.path.dirname(__file__), "test_data")
 
 
-@pytest.mark.parametrize(
-    "params",
-    [
+def test_correct_read():
+    tlpkl_file = pathlib.Path(TEST_DATA, "valid.tlpkl")
+    test_data = nrt.read(tlpkl_file)
+
+    x = [86, 88, 6, 24, 78, 46, 30, 60, 76, 76, 70, 66, 30, 76]
+    y = [26, 2, 94, 40, 52, 58, 62, 86, 16, 60, 82, 44, 18, 94]
+    z = [98, 44, 92, 50, 48, 72, 64, 44, 92, 58, 48, 18, 28, 10]
+    size = [18, 40, 90, 67, 89, 88, 82, 82, 76, 51, 11, 282, 248, 244]
+    metric_best = numpy.array(
         [
-            2,
-            [
-                86,
-                88,
-                6,
-                24,
-                78,
-                46,
-                30,
-                60,
-                76,
-                76,
-                70,
-                66,
-                30,
-                76,
-            ],
+            0.639648,
+            0.945801,
+            0.947754,
+            0.951172,
+            0.953125,
+            0.955566,
+            0.956055,
+            0.956543,
+            0.534180,
+            0.538574,
+            0.550781,
+            0.880371,
+            0.885742,
+            0.888184,
         ],
-        [
-            1,
-            [
-                26,
-                2,
-                94,
-                40,
-                52,
-                58,
-                62,
-                86,
-                16,
-                60,
-                82,
-                44,
-                18,
-                94,
-            ],
-        ],
-    ],
-)
-def test_read_tlpkl_is_valid_x_y(params):
-    idx, values = params
-    tlpkl_file = pathlib.Path(TEST_DATA, "valid.tlpkl")
-    test_data = nrt.read(tlpkl_file)
-
-    numpy.testing.assert_array_equal(test_data.iloc[:, idx], values)
+        dtype=numpy.float32,
+    )
+    width = [37] * len(x)
+    height = [40] * len(x)
+    depth = [50] * len(x)
+    predicted_class = [1] * 8 + [2] * 6
+    predicted_class_name = ["cluster_2.pkl"] * 8 + ["cluster_3.pkl"] * 6
+    expected = pandas.DataFrame(
+        {
+            "X": x,
+            "Y": y,
+            "Z": z,
+            "size": size,
+            "metric_best": metric_best,
+            "predicted_class": predicted_class,
+            "predicted_class_name": predicted_class_name,
+            "width": width,
+            "height": height,
+            "depth": depth,
+        }
+    )
+    pandas.testing.assert_frame_equal(test_data[expected.columns], expected)
 
 
-def test_read_tlpkl_is_valid_z():
-    tlpkl_file = pathlib.Path(TEST_DATA, "valid.tlpkl")
-    test_data = nrt.read(tlpkl_file)
-
-    dim = 100
-    z_values = [
-        dim - entry
-        for entry in [
-            98,
-            44,
-            92,
-            50,
-            48,
-            72,
-            64,
-            44,
-            92,
-            58,
-            48,
-            18,
-            28,
-            10,
-        ]
-    ]
-    numpy.testing.assert_array_equal(test_data.iloc[:, 0], z_values)
-
-
-def test_read_tlp_is_valid_size():
-    tlpkl_file = pathlib.Path(TEST_DATA, "valid.tlpkl")
-    test_data = nrt.read(tlpkl_file)
-
-    size = [
-        18,
-        40,
-        90,
-        67,
-        89,
-        88,
-        82,
-        82,
-        76,
-        51,
-        11,
-        282,
-        248,
-        244,
-    ]
-    numpy.testing.assert_array_equal(test_data.iloc[:, 4], size)
-
-
-def test_read_tlp_is_valid_metric():
-    tlpkl_file = pathlib.Path(TEST_DATA, "valid.tlpkl")
-    test_data = nrt.read(tlpkl_file)
-
-    metric = [
-        0.639648,
-        0.945801,
-        0.947754,
-        0.951172,
-        0.953125,
-        0.955566,
-        0.956055,
-        0.956543,
-        0.534180,
-        0.538574,
-        0.550781,
-        0.880371,
-        0.885742,
-        0.888184,
-    ]
-    numpy.testing.assert_array_almost_equal(test_data.iloc[:, 3], metric)
-
-
-def test_read_tlpkl_missing_dim_z_leads_to_warning():
-    tlpkl_file = pathlib.Path(TEST_DATA, "valid_missing_dim_z.tlpkl")
-    with pytest.warns(nrt.DimZMissingWarning):
-        nrt.read(tlpkl_file)
+def test_correct_prepare_napari_dimz():
+    x = [86, 88, 6]
+    y = [26, 2, 94]
+    z = [98, 44, 92]
+    size = [18, 40, 90]
+    metric_best = numpy.array(
+        [0.639648, 0.945801, 0.947754], dtype=numpy.float32
+    )
+    width = [37] * len(x)
+    height = [40] * len(x)
+    depth = [50] * len(x)
+    input_df = pandas.DataFrame(
+        {
+            "Z": z,
+            "Y": y,
+            "X": x,
+            "size": size,
+            "metric_best": metric_best,
+            "width": width,
+            "height": height,
+            "depth": depth,
+        }
+    )
+    input_df.attrs["dim_z"] = 100
+    expected = pandas.DataFrame(
+        {
+            "z": x,
+            "y": y,
+            "x": [100 - entry for entry in z],
+            "size": size,
+            "metric": metric_best,
+            "boxsize": numpy.mean(numpy.array([width, height, depth])),
+        }
+    )
+    test_df, test_coords, test_metadata = nrt._prepare_napari(input_df)
+    assert test_coords == ["x", "y", "z"]
+    assert test_metadata == ["metric", "size", "boxsize"]
+    pandas.testing.assert_frame_equal(
+        test_df[expected.columns.values], expected
+    )
 
 
 @pytest.mark.filterwarnings(
     "ignore::box_manager.readers.tlpkl.DimZMissingWarning"
 )
-def test_read_tlpkl_missing_dim_z_attr_yields_normal_z():
-    tlpkl_file = pathlib.Path(TEST_DATA, "valid_missing_dim_z.tlpkl")
-    test_data = nrt.read(tlpkl_file)
-
-    z_values = [
-        98,
-        44,
-        92,
-        50,
-        48,
-        72,
-        64,
-        44,
-        92,
-        58,
-        48,
-        18,
-        28,
-        10,
-    ]
-    numpy.testing.assert_array_equal(test_data.iloc[:, 0], z_values)
-
-
-@pytest.mark.parametrize("params", [[6, 40], [5, 50], [7, 37]])
-def test_read_tlp_is_correct_boxsize(params):
-    idx, box_size = params
-    tlpkl_file = pathlib.Path(TEST_DATA, "valid.tlpkl")
-    test_data = nrt.read(tlpkl_file)
-
-    numpy.testing.assert_array_equal(
-        test_data.iloc[:, idx], [box_size] * len(test_data)
+def test_correct_prepare_napari_no_dimz():
+    x = [86, 88, 6]
+    y = [26, 2, 94]
+    z = [98, 44, 92]
+    size = [18, 40, 90]
+    metric_best = numpy.array(
+        [0.639648, 0.945801, 0.947754], dtype=numpy.float32
     )
+    width = [37] * len(x)
+    height = [40] * len(x)
+    depth = [50] * len(x)
+    input_df = pandas.DataFrame(
+        {
+            "Z": z,
+            "Y": y,
+            "X": x,
+            "size": size,
+            "metric_best": metric_best,
+            "width": width,
+            "height": height,
+            "depth": depth,
+        }
+    )
+    expected = pandas.DataFrame(
+        {
+            "z": x,
+            "y": y,
+            "x": z,
+            "size": size,
+            "metric": metric_best,
+            "boxsize": numpy.mean(numpy.array([width, height, depth])),
+        }
+    )
+    test_df, test_coords, test_metadata = nrt._prepare_napari(input_df)
+    assert test_coords == ["x", "y", "z"]
+    assert test_metadata == ["metric", "size", "boxsize"]
+    pandas.testing.assert_frame_equal(
+        test_df[expected.columns.values], expected
+    )
+
+
+def test_read_tlpkl_missing_dim_z_leads_to_warning():
+    x = [86, 88, 6]
+    y = [26, 2, 94]
+    z = [98, 44, 92]
+    size = [18, 40, 90]
+    metric_best = numpy.array(
+        [0.639648, 0.945801, 0.947754], dtype=numpy.float32
+    )
+    width = [37] * len(x)
+    height = [40] * len(x)
+    depth = [50] * len(x)
+    input_df = pandas.DataFrame(
+        {
+            "Z": z,
+            "Y": y,
+            "X": x,
+            "size": size,
+            "metric_best": metric_best,
+            "width": width,
+            "height": height,
+            "depth": depth,
+        }
+    )
+    with pytest.warns(nrt.DimZMissingWarning):
+        nrt._prepare_napari(input_df)
+
+
+def test_to_napari_file_correct_returns():
+    tlpkl_file = pathlib.Path(TEST_DATA, "valid.tlpkl")
+
+    points1, points2 = nrt.to_napari(tlpkl_file)
+    assert all([len(entry) == 3 for entry in (points1, points2)])
+
+    x = [86, 88, 6, 24, 78, 46, 30, 60, 76, 76, 70, 66, 30, 76]
+    y = [26, 2, 94, 40, 52, 58, 62, 86, 16, 60, 82, 44, 18, 94]
+    z = [98, 44, 92, 50, 48, 72, 64, 44, 92, 58, 48, 18, 28, 10]
+    points = pandas.DataFrame(
+        {
+            "x": [100 - entry for entry in z],
+            "y": y,
+            "z": x,
+        }
+    )
+    pandas.testing.assert_frame_equal(points1[0], points.iloc[:8])
+    pandas.testing.assert_frame_equal(points2[0], points.iloc[8:])
+
+    size = [18, 40, 90, 67, 89, 88, 82, 82, 76, 51, 11, 282, 248, 244]
+    metric_best = numpy.array(
+        [
+            0.639648,
+            0.945801,
+            0.947754,
+            0.951172,
+            0.953125,
+            0.955566,
+            0.956055,
+            0.956543,
+            0.534180,
+            0.538574,
+            0.550781,
+            0.880371,
+            0.885742,
+            0.888184,
+        ],
+        dtype=numpy.float32,
+    )
+    width = [37] * len(x)
+    height = [40] * len(x)
+    depth = [50] * len(x)
+
+    boxsize = [numpy.array([width, height, depth]).mean().mean()] * len(x)
+
+    metadata = {
+        "id": 1,
+        "size_min": min(size[:8]),
+        "size_max": max(size[:8]),
+        "metric_min": min(metric_best[:8]),
+        "metric_max": max(metric_best[:8]),
+        "boxsize_min": min(boxsize[:8]),
+        "boxsize_max": max(boxsize[:8]),
+    }
+    assert list(sorted(metadata.keys())) == list(
+        sorted(points1[1]["metadata"].keys())
+    )
+    for key in metadata.keys():
+        val1 = metadata[key]
+        val2 = points1[1]["metadata"][key]
+        numpy.testing.assert_array_almost_equal(val1, val2)
+
+    metadata = {
+        "id": 2,
+        "size_min": min(size[8:]),
+        "size_max": max(size[8:]),
+        "metric_min": min(metric_best[8:]),
+        "metric_max": max(metric_best[8:]),
+        "boxsize_min": min(boxsize[8:]),
+        "boxsize_max": max(boxsize[8:]),
+    }
+    assert list(sorted(metadata.keys())) == list(
+        sorted(points2[1]["metadata"].keys())
+    )
+    for key in metadata.keys():
+        val1 = metadata[key]
+        val2 = points2[1]["metadata"][key]
+        numpy.testing.assert_array_almost_equal(val1, val2)
+
+    features = {
+        "size": size[:8],
+        "metric": metric_best[:8],
+        "boxsize": boxsize[:8],
+    }
+    assert list(sorted(features.keys())) == list(
+        sorted(points1[1]["features"].keys())
+    )
+    for key in features.keys():
+        val1 = features[key]
+        val2 = points1[1]["features"][key]
+        numpy.testing.assert_array_almost_equal(val1, val2)
+
+    features = {
+        "size": size[8:],
+        "metric": metric_best[8:],
+        "boxsize": boxsize[8:],
+    }
+    assert list(sorted(features.keys())) == list(
+        sorted(points2[1]["features"].keys())
+    )
+    for key in features.keys():
+        val1 = features[key]
+        val2 = points2[1]["features"][key]
+        numpy.testing.assert_array_almost_equal(val1, val2)
+
+    numpy.testing.assert_array_equal(boxsize[:8], points1[1]["size"])
+    numpy.testing.assert_array_equal(boxsize[8:], points2[1]["size"])
--- src/box_manager/readers/tlpkl.py
+++ src/box_manager/readers/tlpkl.py
@@ -1,51 +1,123 @@
+import os
 import typing
 import warnings
 
+import matplotlib.cm as mcm
+import numpy as np
 import pandas as pd
 
 if typing.TYPE_CHECKING:
-    import pathlib
 
-    import numpy as np
+    import numpy.typing as npt
 
 
 class DimZMissingWarning(Warning):
     pass
 
 
-def read(path: "pathlib.Path") -> pd.DataFrame:
-    pandas_data: pd.DataFrame = pd.read_pickle(path)
+def read(path: "os.PathLike") -> pd.DataFrame:
+    """
+    Read a tlpkl conform file into memory.
+    tlpkl files need to have their coordinates for the Z and X dimension inverted.
 
-    if "dim_z" not in pandas_data.attrs:
-        warnings.warn(
-            "dim_z attribute missing in pkl file! Please invert your Z dimension values manually!",  # noqa: E501
-            DimZMissingWarning,
-        )
-    else:
-        pandas_data["Z"] = pandas_data.attrs["dim_z"] - pandas_data["Z"]
+    :param path: Path of the file to read information from.
+    :type path: Path object or str
+
+    :return: DataFrame containing the coordinates, box size, metrics, and cluster size
+    :rtype: Pandas DataFrame
+    """
+    pandas_data: pd.DataFrame = pd.read_pickle(path)
 
     pandas_data["X"] = pandas_data["X"].astype(int)
     pandas_data["Y"] = pandas_data["Y"].astype(int)
     pandas_data["Z"] = pandas_data["Z"].astype(int)
 
-    pandas_data.rename(
-        columns={
-            "X": "z",
-            "Y": "y",
-            "Z": "x",
-            "width": "box_z",
-            "height": "box_y",
-            "depth": "box_x",
-        },
-        inplace=True,
+    return pandas_data
+
+
+def _prepare_napari(
+    input_data: pd.DataFrame,
+) -> tuple[pd.DataFrame, list[str], list[str]]:
+    rename_columns = {
+        "X": "z",
+        "Y": "y",
+        "Z": "x",
+        "width": "boxsize",
+        "metric_best": "metric",
+    }
+    output_data: pd.DataFrame | None = input_data.rename(
+        columns=rename_columns
     )
 
-    return pandas_data[
-        ["x", "y", "z", "metric_best", "size", "box_x", "box_y", "box_z"]
-    ]
+    if output_data is None:
+        assert False, "Inplace option cannot be enabled"
+
+    if "dim_z" not in output_data.attrs:
+        warnings.warn(
+            "dim_z attribute missing in pkl file! Please invert your x dimension values manually!",  # noqa: E501
+            DimZMissingWarning,
+        )
+    else:
+        output_data["x"] = output_data.attrs["dim_z"] - output_data["x"]
+    output_data["boxsize"] = (
+        output_data[["height", "boxsize", "depth"]].mean().mean()
+    )
+
+    return output_data, ["x", "y", "z"], ["metric", "size", "boxsize"]
 
 
 def to_napari(
-    path: "pathlib.Path",
-) -> "tuple[np.ndarray, dict[str, typing.Any], dict[str, typing.Any], str]":
-    return read(path), {}, {}, "points"
+    input_data: "os.PathLike | pd.DataFrame",
+) -> "list[tuple[npt.ArrayLike, dict[str, typing.Any], str]]":
+    """
+    Read a tlpkl conform file into memory to use within napari.
+
+    :param path: Path of the file to read information from.
+    :type path: pathlib.Path or str
+
+    :return: Data to create a point, i.e., coords, point_kwargs, and type
+    :rtype: list[tuple[npt.ArrayLike, dict[str, typing.Any], str]]
+    """
+    if isinstance(input_data, os.PathLike):
+        input_data = read(input_data)
+    data, coords_idx, metadata_idx = _prepare_napari(input_data)
+
+    group_name = "predicted_class_name"
+    colors = mcm.get_cmap("gist_rainbow")
+    n_classes = np.unique(input_data[group_name]).size
+
+    output_list: "list[tuple[npt.ArrayLike, dict[str, typing.Any], str]]" = []
+    for idx, (cluster_name, cluster_df) in enumerate(
+        data.sort_values(by=["predicted_class", "metric"]).groupby(
+            group_name, sort=False
+        )
+    ):
+        color = colors(idx / (n_classes - 1))
+        metadata = {
+            f"{entry}_{func.__name__}": func(cluster_df[entry])
+            for func in [min, max]
+            for entry in metadata_idx
+        }
+        metadata["id"] = cluster_df["predicted_class"].iloc[0]
+
+        # to_numpy currently needed. Should be fixed in 0.4.16rc8
+        features = {
+            entry: cluster_df[entry].to_numpy() for entry in metadata_idx
+        }
+
+        point_kwargs = {
+            "edge_color": [color],
+            "face_color": "transparent",
+            "symbol": "disc",
+            "edge_width": 2,
+            "edge_width_is_relative": False,
+            "size": cluster_df["boxsize"],
+            "out_of_slice_display": True,
+            "opacity": 0.5,
+            "name": cluster_name,
+            "metadata": metadata,
+            "features": features,
+        }
+        output_list.append((cluster_df[coords_idx], point_kwargs, "points"))
+
+    return output_list

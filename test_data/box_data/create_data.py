9bc7d9186f997edce686dfdbc426fc8b751d1b24
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2022-08-01 11:37:04 +0200

    update test_data

--- test_data/box_data/create_data.py
+++ test_data/box_data/create_data.py
@@ -5,6 +5,7 @@ import numpy as np
 
 OUTPUT_DIR = os.path.dirname(__file__)
 NDIM = 50
+np.random.seed(42)
 
 for mic_id in range(10):
     data = np.zeros((NDIM, NDIM), dtype=np.float32)
@@ -12,7 +13,7 @@ for mic_id in range(10):
     for box_id in range(3):
         x_coord = int(np.random.random() * NDIM)
         y_coord = int(np.random.random() * NDIM)
-        coords.append((x_coord, y_coord, 0, 0))
+        coords.append((y_coord, x_coord, 0, 0))
         data[x_coord, y_coord] = 1
 
     with mrcfile.new(

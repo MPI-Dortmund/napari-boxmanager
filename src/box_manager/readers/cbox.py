2fbb3b902be10ffb5fdce3941b5937f17099ceb9
thorsten.wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2022-08-30 09:15:28 +0200

    start skeleton cbox reader/writer

--- src/box_manager/readers/cbox.py
+++ src/box_manager/readers/cbox.py
@@ -3,75 +3,57 @@ import typing
 from pyStarDB import sp_pystardb as star
 import pandas as pd
 import numpy as np
-from interface import ReaderInterface
+
+valid_extensions = ['cbox']
+coords_3d_idx = ["x", "y", "z"]
+meta_idx = ["confidence", "size"]
+
+def read(path: os.PathLike) -> typing.Dict:
+    starfile = star.StarFile(path)
+    data_dict = starfile['cryolo']
+    return data_dict
+
+def to_napari(
+    path: os.PathLike | list[os.PathLike],
+) -> "list[tuple[npt.ArrayLike, dict[str, typing.Any], str]]":
+
+    if not isinstance(path, list):
+        path = sorted(glob.glob(path))  # type: ignore
+
+    for file_name in path:
+        pass
+
+def get_valid_extensions() -> list[str]:
+    return valid_extensions
+
+def from_napari(
+    path: os.PathLike | list[os.PathLike] | pd.DataFrame,
+    data: typing.Any,
+    meta: dict,
+):
+    data_dict = read(path)
+    napari_df = _prepare_napari(data_dict)
 
 
+def _prepare_napari(input_dict: typing.Dict) -> pd.DataFrame:
 
-class CBOXReader(ReaderInterface):
+    output_data: pd.DataFrame = pd.DataFrame(
+        columns=_get_3d_coords_idx()
+        + _get_meta_idx()
+    )
+    cryolo_data = input_dict['cryolo']
 
-    def __init__(self):
-        self.valid_extensions = ['cbox']
-        self.coords_3d_idx = ["x", "y", "z"]
-        self.meta_idx = ["confidence", "size"]
-        self.idx_cbox_dict = {
-            'x': '_CoordinateZ',
-            'y': '_CoordinateY',
-            'z': '_CoordinateX',
-            'boxsize': '_Width',
-            'confidence': '_Confidence',
-            'size': ['_EstWidth','_EstHeight'],
-            'numboxes': ['_NumBoxes'],
-            'angle': ['_Angle']
-        }
+    output_data['x'] = cryolo_data['_CoordinateZ']
+    output_data['y'] = cryolo_data['_CoordinateY']
+    output_data['z'] = cryolo_data['_CoordinateX']
+    output_data["boxsize"] = (np.array(cryolo_data["_Width"]) + np.array(cryolo_data["_Height"]))/2
 
-    def read(self, path: os.PathLike) -> typing.Dict:
-        starfile = star.StarFile(path)
-        data_dict = starfile['cryolo']
-        return data_dict
+    print(cryolo_data['_CoordinateZ'])
 
-    def to_napari(
-        self,
-        path: os.PathLike | list[os.PathLike],
-    ) -> "list[tuple[npt.ArrayLike, dict[str, typing.Any], str]]":
-
-        if not isinstance(path, list):
-            path = sorted(glob.glob(path))  # type: ignore
-
-        for file_name in path:
-            pass
-
-    def get_valid_extensions(self) -> list[str]:
-        return self.valid_extensions
-
-    def from_napari(
-        self,
-        path: os.PathLike | list[os.PathLike] | pd.DataFrame,
-        data: typing.Any,
-        meta: dict,
-    ):
-        data_dict = self.read(path)
-        napari_df = self._prepare_napari(data_dict)
-        print(napari_df)
-
-    def _prepare_napari(self, input_dict: typing.Dict) -> pd.DataFrame:
-
-        output_data: pd.DataFrame = pd.DataFrame(
-            columns=self._get_3d_coords_idx()
-            + self._get_meta_idx()
-        )
-        cryolo_data = input_dict['cryolo']
-
-        output_data['x'] = cryolo_data['_CoordinateZ']
-        output_data['y'] = cryolo_data['_CoordinateY']
-        output_data['z'] = cryolo_data['_CoordinateX']
-        output_data["boxsize"] = (np.array(cryolo_data["_Width"]) + np.array(cryolo_data["_Height"]))/2
-
-        print(cryolo_data['_CoordinateZ'])
-
-    def _get_3d_coords_idx(self):
-        return COORDS_3D_IDX
+def _get_3d_coords_idx():
+    return coords_3d_idx
 
 
-    def _get_meta_idx(self):
-        return META_IDX
+def _get_meta_idx():
+    return meta_idx
 

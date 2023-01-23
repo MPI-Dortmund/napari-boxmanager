7769da75943145ae4757f8b7ca60fbe28238bc32
thorstenwagner <twa1@posteo.de>
2023-01-23 15:18:28 +0100

    fix tests

--- src/box_manager/_tests/test_cbox.py
+++ src/box_manager/_tests/test_cbox.py
@@ -1,12 +1,13 @@
 import box_manager._reader as br
 import pytest
-
+import os
+HERE = os.path.dirname(__file__)
 def test_open_2D_filament_cbox_files_including_empty():
     reader = br.napari_get_reader("fake.cbox")
-    reader("/home/twagner/Projects/napari/src/napari-boxmanager-github/test_data/filament_cbox_2d/*.cbox")
+    reader(f"{HERE}/../../../test_data/filament_cbox_2d/*.cbox")
 
 @pytest.mark.parametrize("input", [
-"/home/twagner/Projects/napari/src/napari-boxmanager-github/test_data/filament_cbox_2d/A*.cbox",
+f"{HERE}/../../../test_data/filament_cbox_2d/A*.cbox",
 ])
 def test_open_2D_filament_cbox_files_without_empty(input):
     reader = br.napari_get_reader(input)

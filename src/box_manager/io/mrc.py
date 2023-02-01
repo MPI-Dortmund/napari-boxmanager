26e1c7636a4e75cbd417ae1208cfa5de775570a7
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2023-02-01 12:11:49 +0100

    Add mrci file conversion

--- src/box_manager/io/mrc.py
+++ src/box_manager/io/mrc.py
@@ -1,20 +1,26 @@
-import glob
 import os
 import typing
 
 import mrcfile
 import numpy as np
 import pandas as pd
-from .io_utils import MAX_LAYER_NAME, PROXY_THRESHOLD_GB, LoaderProxy, to_napari_image
+
+from .io_utils import to_napari_image
 
 if typing.TYPE_CHECKING:
     import numpy.typing as npt
 
 
 def load_image(path: str) -> np.array:
+
+    extension = os.path.splitext(path)[1]
+
     with mrcfile.open(path, "r", permissive=True) as mrc:
         data = mrc.data
-    return data
+    if extension == ".mrci":
+        return data.astype(np.int32)
+    else:
+        return data
 
 
 def get_pixel_size(path: str) -> float:
@@ -28,11 +34,31 @@ def to_napari(
     path: os.PathLike | list[os.PathLike],
 ) -> "list[tuple[npt.ArrayLike, dict[str, typing.Any], str]]":
 
-    return to_napari_image(path, load_image=load_image, get_pixel_size=get_pixel_size)
+    if isinstance(path, list):
+        extension = os.path.splitext(path[0])[1]
+    else:
+        extension = os.path.splitext(path)[1]
+
+    if extension == ".mrci":
+        do_normalize = False
+    else:
+        do_normalize = True
+
+    layer_data = to_napari_image(
+        path,
+        load_image=load_image,
+        get_pixel_size=get_pixel_size,
+        do_normalize=do_normalize,
+    )
+
+    if extension == ".mrci":
+        layer_data = [(_[0], _[1], "labels") for _ in layer_data]
+
+    return layer_data
 
 
 def get_valid_extensions():
-    return ["mrc", "mrcs", "st", "rec"]
+    return ["mrc", "mrcs", "st", "rec", "mrci"]
 
 
 def from_napari(

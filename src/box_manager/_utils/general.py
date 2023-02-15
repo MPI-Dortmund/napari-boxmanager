2e8beb3c9359a6b398376df27acf9b3391b5315a
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2023-02-15 16:04:56 +0100

    Fix all nan series for min/max

--- src/box_manager/_utils/general.py
+++ src/box_manager/_utils/general.py
@@ -1,17 +1,30 @@
 import napari
 import numpy as np
+import pandas
 
 
 def get_min_floor(vals, step=1000):
     try:
-        return np.round(np.floor(np.nanmin(vals) * step) / step, 3)
+        if isinstance(vals, pandas.Series):
+            if pandas.isnull(vals).all():
+                return np.nan
+            else:
+                return np.round(np.floor(np.nanmin(vals) * step) / step, 3)
+        else:
+            return np.round(np.floor(np.nanmin(vals) * step) / step, 3)
     except ValueError:
         return np.nan
 
 
 def get_max_floor(vals, step=1000):
     try:
-        return np.round(np.ceil(np.nanmax(vals) * step) / step, 3)
+        if isinstance(vals, pandas.Series):
+            if pandas.isnull(vals).all():
+                return np.nan
+            else:
+                return np.round(np.floor(np.nanmax(vals) * step) / step, 3)
+        else:
+            return np.round(np.floor(np.nanmax(vals) * step) / step, 3)
     except ValueError:
         return np.nan
 

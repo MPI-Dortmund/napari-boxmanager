3e9d55cbc587324f61ea377fadd059d93b0eb586
thorstenwagner <twa1@posteo.de>
2023-03-15 15:05:48 +0100

    use get_max_ceil instead of get_max_floor

--- src/box_manager/_utils/general.py
+++ src/box_manager/_utils/general.py
@@ -16,15 +16,15 @@ def get_min_floor(vals, step=1000):
         return np.nan
 
 
-def get_max_floor(vals, step=1000):
+def get_max_ceil(vals, step=1000):
     try:
         if isinstance(vals, pandas.Series):
             if pandas.isnull(vals).all():
                 return np.nan
             else:
-                return np.round(np.floor(np.nanmax(vals) * step) / step, 3)
+                return np.round(np.ceil(np.nanmax(vals) * step) / step, 3)
         else:
-            return np.round(np.floor(np.nanmax(vals) * step) / step, 3)
+            return np.round(np.ceil(np.nanmax(vals) * step) / step, 3)
     except ValueError:
         return np.nan
 

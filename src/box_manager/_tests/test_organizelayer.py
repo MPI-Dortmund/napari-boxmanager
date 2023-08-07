8dbd1fd013dfa02eff4f9ad00ff2f44e00679a0d
thorstenwagner <twa1@posteo.de>
2023-08-07 15:14:23 +0200

    test if cbox get written also in 2D

--- src/box_manager/_tests/test_organizelayer.py
+++ src/box_manager/_tests/test_organizelayer.py
@@ -7,6 +7,9 @@ import pytest
 import mrcfile
 import tempfile
 
+import os
+import shutil
+
 @pytest.fixture(scope="function")
 def napari_viewer():
     yield napari.Viewer()
@@ -33,7 +36,7 @@ def organize_layer_widget_stack(napari_viewer):
         with mrcfile.new(f"{tmpdirname}/tmp_2.mrc") as mrc:
             mrc.set_data(rand_vol)
         napari_viewer.open(plugin='napari-boxmanager',
-                    path=f"'{tmpdirname}/*.mrc'")
+                    path=[f"{tmpdirname}/tmp.mrc", f"{tmpdirname}/tmp_2.mrc"], stack=True)
         widget, _ = napari_viewer.window.add_plugin_dock_widget('napari-boxmanager', widget_name='organize_layer', tabify=True)
         widget.widget()._new_shapes()
         yield widget.widget()
@@ -47,14 +50,19 @@ def organize_layer_widget_stack(napari_viewer):
 class Test__run_save:
     def test_save_tomo_filament(self, napari_viewer, organize_layer_widget_tomo):
         # generate random tomogram
+        os.makedirs("/tmp/blub/", exist_ok=True)
         fila = [[77, 50, 50], [77, 100, 100], [77, 200, 200]]
         napari_viewer.layers[1]._add_shapes(fila, shape_type='path', edge_width=100)
         organize_layer_widget_tomo._run_save()
+        assert os.path.exists('/tmp/blub/tmp.cbox') == True
+        shutil.rmtree('/tmp/blub/')
 
-    def test_save_stack_filament(self, napari_viewer, organize_layer_widget_tomo):
+    def test_save_stack_filament(self, napari_viewer, organize_layer_widget_stack):
         # generate random tomogram
-        fila = [[249, 50, 50], [249, 100, 100], [249, 200, 200]]
+        os.makedirs("/tmp/blub/", exist_ok=True)
+        fila = [[0, 50, 50], [0, 100, 100], [0, 200, 200]]
         napari_viewer.layers[1]._add_shapes(fila, shape_type='path', edge_width=100)
-        organize_layer_widget_tomo._run_save()
+        organize_layer_widget_stack._run_save()
 
-        #assert True==False
+        assert os.path.exists('/tmp/blub/tmp.cbox') == True
+        shutil.rmtree('/tmp/blub/')

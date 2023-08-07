5ec0680432c4524a0261274309a2ca25450023a8
thorstenwagner <twa1@posteo.de>
2023-08-07 14:45:13 +0200

    add framebuffer

--- src/box_manager/_tests/test_organizelayer.py
+++ src/box_manager/_tests/test_organizelayer.py
@@ -23,6 +23,21 @@ def organize_layer_widget_tomo(napari_viewer):
         widget, _ = napari_viewer.window.add_plugin_dock_widget('napari-boxmanager', widget_name='organize_layer', tabify=True)
         widget.widget()._new_shapes()
         yield widget.widget()
+
+@pytest.fixture(scope="function")
+def organize_layer_widget_stack(napari_viewer):
+    rand_vol = np.random.randn(500,500).astype(np.float32)
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        with mrcfile.new(f"{tmpdirname}/tmp.mrc") as mrc:
+            mrc.set_data(rand_vol)
+        with mrcfile.new(f"{tmpdirname}/tmp_2.mrc") as mrc:
+            mrc.set_data(rand_vol)
+        napari_viewer.open(plugin='napari-boxmanager',
+                    path=f"'{tmpdirname}/*.mrc'")
+        widget, _ = napari_viewer.window.add_plugin_dock_widget('napari-boxmanager', widget_name='organize_layer', tabify=True)
+        widget.widget()._new_shapes()
+        yield widget.widget()
+
 @patch(
         "qtpy.QtWidgets.QFileDialog.getExistingDirectory",
         MagicMock(
@@ -33,6 +48,13 @@ class Test__run_save:
     def test_save_tomo_filament(self, napari_viewer, organize_layer_widget_tomo):
         # generate random tomogram
         fila = [[77, 50, 50], [77, 100, 100], [77, 200, 200]]
-        assert len(napari_viewer.layers) == 2
         napari_viewer.layers[1]._add_shapes(fila, shape_type='path', edge_width=100)
         organize_layer_widget_tomo._run_save()
+
+    def test_save_stack_filament(self, napari_viewer, organize_layer_widget_tomo):
+        # generate random tomogram
+        fila = [[249, 50, 50], [249, 100, 100], [249, 200, 200]]
+        napari_viewer.layers[1]._add_shapes(fila, shape_type='path', edge_width=100)
+        organize_layer_widget_tomo._run_save()
+
+        #assert True==False

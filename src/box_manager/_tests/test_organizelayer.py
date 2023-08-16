c197196e05e5a9e625685fbfffa1c3e4174b1027
thorstenwagner <twa1@posteo.de>
2023-08-16 10:18:19 +0200

    add more unit tests and fix the cbox bug.

--- src/box_manager/_tests/test_organizelayer.py
+++ src/box_manager/_tests/test_organizelayer.py
@@ -59,15 +59,26 @@ class Test__run_save:
         assert os.path.exists('/tmp/blub/tmp.cbox') == True
         shutil.rmtree('/tmp/blub/')
 
-    def test_save_tomo_particle(self, napari_viewer, organize_layer_widget_tomo):
+    def test_save_tomo_particle_cbox(self, napari_viewer, organize_layer_widget_tomo):
         # generate random tomogram
         organize_layer_widget_tomo._new_points()
         organize_layer_widget_tomo.save_layers['format'].setCurrentIndex(2) #cbox
         os.makedirs("/tmp/blub/", exist_ok=True)
-        points = [[77, 50, 50], [77, 100, 100], [77, 200, 200]]
+        points = [[77, 0, 0], [77, 100, 100], [77, 200, 200], [77, 300, 300]]
         napari_viewer.layers[1].add(points)
         organize_layer_widget_tomo._run_save()
         assert os.path.exists('/tmp/blub/tmp.cbox') == True
+        shutil.rmtree('/tmp/blub/')
+
+    def test_save_tomo_particle_coords(self, napari_viewer, organize_layer_widget_tomo):
+        # generate random tomogram
+        organize_layer_widget_tomo._new_points()
+        organize_layer_widget_tomo.save_layers['format'].setCurrentIndex(0) #coords
+        os.makedirs("/tmp/blub/", exist_ok=True)
+        points = [[77, 0, 0], [77, 100, 100], [77, 200, 200]]
+        napari_viewer.layers[1].add(points)
+        organize_layer_widget_tomo._run_save()
+        assert os.path.exists('/tmp/blub/tmp.coords') == True
         shutil.rmtree('/tmp/blub/')
 
     def test_save_stack_filament(self, napari_viewer, organize_layer_widget_stack):
